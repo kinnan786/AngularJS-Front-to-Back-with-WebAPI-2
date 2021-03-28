24174cfbc8c39a86172c334269bc725609b57bce
Kinnan Nawaz <kinnannawaz786@gmail.com>
2021-03-28 10:55:07 +0500

    ODATA Changes

--- APM.Client/app/products/productListCtrl.js
+++ APM.Client/app/products/productListCtrl.js
@@ -9,8 +9,25 @@
     function ProductListCtrl(productResource) {
         var vm = this;
 
-        productResource.query(function (data) {
-            vm.products = data;
-        });
+        productResource.query({
+            $filter: "contains(ProductCode, 'GDN') and Price ge 5 and Price le 20",
+            $orderby: "Price desc"},
+            function (data) {
+                vm.products = data;
+            });
+
+        // Alternative code using variables instead of hard-coded values
+        //vm.searchCriteria = "GDN";
+        //vm.sortProperty = "Price";
+        //vm.sortDirection = "desc";
+
+        //productResource.query({
+        //    $filter: "contains(ProductCode, '" + vm.searchCriteria + "')" +
+        //        " or contains(ProductName, '" + vm.searchCriteria + "')",
+        //    $orderby: vm.sortProperty + " " + vm.sortDirection
+        //}, function (data) {
+        //    vm.products = data;
+        //})
+
     }
 }());
